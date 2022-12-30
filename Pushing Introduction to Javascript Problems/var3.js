90d540d8c717fe5a277bed614c85b7926bccf1aa
akshayaravindran <akshayaravindran1996@gmail.com>
2022-12-30 13:57:25 +0530

    Pushing Introduction to Javascript Problems

---
+++ Pushing Introduction to Javascript Problems/var3.js
@@ -0,0 +1,19 @@
+// Define a variable called name with your Name as the assigned value
+// Print the value stored in the variable name
+// Change the variable to store your father's name
+// Print the value stored in the variable name
+// Change the variable again to store your mother's name.
+// Print the value stored in the variable name
+
+
+let name = "Akshaya";
+
+console.log(name);
+
+father_name = "Ravindran";
+
+console.log(father_name);
+
+mother_name = "Shyama"
+
+console.log(mother_name);
---
+++ Pushing Introduction to Javascript Problems/var5.js
@@ -0,0 +1,29 @@
+// Store the name, school, grade, section, rollno and the marks scored by the student in 3 subjects
+// Print the report card of the student (You can make it look nice by using some keyboard symbols )
+
+
+let name = "Akshaya";
+
+school = "Crescent public school";
+
+grade = "10th grade";
+
+section = "A";
+
+rollno = 3;
+
+subject1 = 80;
+
+subject2 = 90;
+
+subject3 = 100;
+
+
+console.log ('name    :', name);
+console.log ('school  :', school);
+console.log ('grade   :', grade);
+console.log ('section :', section);
+console.log ('rollno  :', rollno);
+console.log ('subject1:', subject1);
+console.log ('subject2:', subject2);
+console.log ('subject3:', subject3);
